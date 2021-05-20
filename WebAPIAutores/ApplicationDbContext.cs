0e9ff840a7f9c04893e8841979d9faf8b777aec2
Nicolas Bertini Argañaras <nicolas_bertini@hotmail.com>
2021-05-20 20:03:31 -0300

    Add project files.

---
+++ WebAPIAutores/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPIAutores.Entidades;
+
+namespace WebAPIAutores
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions option): base(option)
+        {
+
+        }
+
+
+
+        //Configuramos las tablas que vamos a generar
+        public DbSet<Autor> Autores { get; set; } //Creame una tabla Autores con el esquema de Autor
+        public DbSet<Libro> Libros { get; set; }
+
+    }
+}
---
+++ WebAPIAutores/Controllers/LibrosController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPIAutores.Entidades;
+
+namespace WebAPIAutores.Controllers
+{
+    [ApiController]
+    [Route("api/libros")]
+    public class LibrosController :ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+        public LibrosController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<Libro>> Get(int id)
+        {
+            return await context.Libros.Include(x=>x.Autor).FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post(Libro libro)
+        {
+            //Corroboramos si existe el autor
+            var existeAutor = await context.Autores.AnyAsync(x => x.Id == libro.AutorId);
+            if (!existeAutor)
+            {
+                return BadRequest($"El autor con el id: {libro.AutorId} NO existe.");
+            }
+            context.Add(libro);
+            await context.SaveChangesAsync();
+            return Ok();
+        }
+    }
+}
